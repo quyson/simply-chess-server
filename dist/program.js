3197591df998665481d5e9d3e17399a46c7eb602
Quyson <quyson.nguyen.1999@gmail.com>
2023-11-30 16:08:48 -0800

    Add mssql code to program.ts

--- dist/program.js
+++ dist/program.js
@@ -1,5 +1,15 @@
-import express from "express";
-import morgan from "morgan";
-const app = express();
-app.use(morgan("dev"));
-//# sourceMappingURL=program.js.map
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const app = (0, express_1.default)();
+const sql = require("msnodesqlv8");
+app.use((0, morgan_1.default)("dev"));
+const connectionString = "server=.;Database=Master;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
+const query = "SELECT name FROM sys.databases";
+sql.query(connectionString, query, (err, rows) => {
+    console.log(rows);
+});
--- src/program.ts
+++ src/program.ts
@@ -1,5 +1,16 @@
 import express from "express";
 import morgan from "morgan";
+import { SqlClient } from "msnodesqlv8/types";
+
 const app = express();
+const sql: SqlClient = require("msnodesqlv8");
 
 app.use(morgan("dev"));
+
+const connectionString =
+  "server=.;Database=Master;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
+const query = "SELECT name FROM sys.databases";
+
+sql.query(connectionString, query, (err, rows) => {
+  console.log(rows);
+});
