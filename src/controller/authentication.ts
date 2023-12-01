08f86648b82e83055ed4ab3b215866e3c8dc04c6
Quyson <quyson.nguyen.1999@gmail.com>
2023-11-30 23:13:45 -0800

    Create Login Controller

--- src/controller/authentication.ts
+++ src/controller/authentication.ts
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+import { Request, Response, NextFunction } from "express";
 const jwt = require("jsonwebtoken");
 import AppDataSource from "../program";
 import { UserEntity } from "../entity/userEntity";
@@ -7,7 +8,9 @@ import User from "../interface/models/user";
 require("dotenv").config();
 import CustomError from "../config/error";
 
-const register = async (req, res, next) => {
+const secretOrKey = process.env.SECRET_KEY;
+
+const register = async (req: Request, res: Response, next: NextFunction) => {
   const userRepository = AppDataSource.getRepository(UserEntity);
   const foundUser = await userRepository.findOne({
     where: { username: req.body.username },
@@ -18,14 +21,7 @@ const register = async (req, res, next) => {
     res.status(err.status).send(err.message);
   }
 
-  const hashedPassword = bcrypt
-    .hash(req.body.password, 10)
-    .then((hashedPassword) => {
-      return hashedPassword;
-    })
-    .catch((error) => {
-      next(error);
-    });
+  const hashedPassword: string = await bcrypt.hash(req.body.password, 10);
 
   const newUser = new UserEntity();
   newUser.username = req.body.username;
@@ -45,3 +41,31 @@ const register = async (req, res, next) => {
       next(error);
     });
 };
+
+const Login = async (req: Request, res: Response, next: NextFunction) => {
+  const userRepository = AppDataSource.getRepository(UserEntity);
+  const result = await userRepository.findOne({
+    where: { username: req.body.username },
+  });
+  if (!result) {
+    const err = new CustomError("Cannot find User!", 404);
+    res.status(err.status).send(err.message);
+  }
+  const match: boolean = await bcrypt.compare(
+    req.body.password,
+    result!.password
+  );
+  if (match) {
+    const payload = { id: result!.id, username: result!.username };
+    const token: string = jwt.sign(payload, secretOrKey, { expiresIn: "1d" });
+    return res.status(200).send({
+      message: "Logged in successfully",
+      token: "Bearer " + token,
+    });
+  } else {
+    const err = new CustomError("Username or Password does not match!", 401);
+    res.status(err.status).send(err.message);
+  }
+};
+
+export { register, Login };
